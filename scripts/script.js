a594eab1abf12cd25f9c3fc1bdd03a2ce65535ca
alejandro <alejandro.rodriguez1900@gmail.com>
2025-01-31 13:50:31 +0100

    Botón de cerrar formulario

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 // Obtener el botón de mostrar formulario y el contenedor del formulario
 const showFormButton = document.getElementById('show-form-btn');
 const taskFormContainer = document.getElementById('task-form-container');
+const closeFormButton = document.getElementById('close-form-btn');
 
 // Función para mostrar y ocultar el formulario
 showFormButton.addEventListener('click', function() {
@@ -11,7 +12,11 @@ showFormButton.addEventListener('click', function() {
     taskFormContainer.style.display = "none";   // Ocultar el formulario
     }
 });
-      
+
+// Función para cerrar el formulario cuando se haga clic en el botón de cerrar
+closeFormButton.addEventListener('click', function() {
+    taskFormContainer.style.display = "none";   // Ocultar el formulario
+  });
 
 
 let nombreTarea=document.getElementById("task-name").value;
